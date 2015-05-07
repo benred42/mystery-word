dc041b73858764c8f7d0e6728c9dd60f8b625326
Benjamin Phillips <benred42@gmail.com>
2015-05-07 14:32:37 -0400

    Added mystery_word.py for pure functions

---
+++ mystery_word.py
@@ -0,0 +1,32 @@
+import random
+
+
+def easy_words(a_list):
+    easy_list = [word for word in a_list if len(word) <= 6 and len(word) > 3]
+    return easy_list
+
+
+def medium_words(a_list):
+    medium_list = [word for word in a_list if len(word) <= 8 and len(word) > 5]
+    return medium_list
+
+
+def hard_words(a_list):
+    return [word for word in a_list if len(word) > 7]
+
+
+def random_word(a_list):
+    return random.choice(a_list)
+
+
+def display_word(word, guesses_list):
+    display_list = [letter.upper() for letter in word]
+    for index in range(len(display_list)):
+        if word[index] not in guesses_list:
+            display_list[index] = "_"
+    return " ".join(display_list)
+
+
+def is_word_complete(word, guesses_list):
+    check_list = [letter for letter in word if letter not in guesses_list]
+    return check_list == []
