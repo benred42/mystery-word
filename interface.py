05b3753cac579e4c13d493469666b8e131ab13a4
Benjamin Phillips <benred42@gmail.com>
2015-05-07 23:12:11 -0400

    Changed interface.py to new version

--- interface.py
+++ interface.py
@@ -1,61 +1,144 @@
-from mystery_word import *
+import mystery_word
 import os
 
-def main():
-    welcome()
-    words = import_words("/usr/share/dict/words")
-    word_list = ask_difficulty(words)
+def clean_text(text):
+    return [line.strip().lower() for line in text]
 
-
-
-
-###############################################################################
 
 def import_words(filepath):
     with open(filepath) as file:
         text = file.readlines()
 
-    return clean_text(text)
+        return clean_text(text)
 
-###############################################################################
 
-def clean_text(text):
-    return [line.strip().lower() for line in text]
+def choose_difficulty():
+    words = import_words('/usr/share/dict/words')
+    difficulty = input("What difficulty would you like? [E]asy, [M]edium, or [H]ard? ").lower()
 
-###############################################################################
-
-def ask_difficulty(words):
-    user_input = input("Which difficulty would you like? \
-    [E]asy [M]edium [H]ard [Q]uit? ")
-    user_input = user_input.lower()
-    word_list = []
-
-    if user_input not in "emh":
-        ask_difficulty()
-    os.system('clear')
-
-    elif user_input == "e":
-        word_list = easy_words(words)
-    elif user_input == "m":
-        word_list = medium_words(words)
+    if difficulty == 'e':
+        return mystery_word.easy_words(words)
+    elif difficulty == 'm':
+        return mystery_word.medium_words(words)
+    elif difficulty == 'h':
+        return mystery_word.hard_words(words)
     else:
-        word_list = hard_words(words)
+        print("That is not a valid choice. Please choose again.")
+        return choose_difficulty()
 
-    return word_list
+
+def choose_word(word_list):
+    return mystery_word.random_word(word_list)
 
 
-###############################################################################
+def guess(word_choice):
+    word = word_choice
+    incorrect_guesses = []
+    correct_guesses = []
 
-def number_of_letters(word):
-    print("Your word has {} letters".format(str(len(word))))
+    while win_lose(word, correct_guesses, incorrect_guesses):
+        guess = guess_input((correct_guesses + incorrect_guesses))
+
+        if guess != 'quit':
+            if check_guess(word, guess):
+                correct_guesses.append(guess.upper())
+            else:
+                incorrect_guesses.append(guess.upper())
+        else:
+            os.system('clear')
+            print("Goodbye!  Thanks for Playing!")
+            break
+
+        os.system('clear')
+        guess_display(word, correct_guesses, incorrect_guesses)
+
+
+def guess_input(guess_list):
+    guess = input("(Type \"quit\" to exit) Please guess a letter >>> ").lower()
+
+    if guess == 'quit' or (guess.isalpha() and len(guess) < 2):
+        if guess not in guess_list:
+            return guess
+        else:
+            print("You already guessed that letter!")
+            return guess_input(guess_list)
+    else:
+        print "Invalid guess.  Please try again."
+        return guess_input(guess_list)
+
+
+def check_guess(word, guess):
+    if guess in word:
+        return True
+    else:
+        return False
+
+
+def guess_display(word, correct_guesses, incorrect_guesses):
+
+    number_incorrect = str(len(incorrect_guesses))
+    incorrect = " ".join(incorrect_guesses)
+    print("Incorrect guesses({}/8): [{}]\n".format(number, incorrect.upper()))
+
+    print mystery_word.display_word(word, correct_guesses) + "\n"
+
+
+def win_lose(word, correct_guesses, incorrect_guesses):
+    if mystery_word.is_word_complete(word, correct_guesses):
+        return you_win(word)
+    elif len(incorrect_guesses) > 7:
+        return you_lose(word)
+    else:
+        return True
+
+
+def you_win(word):
+    os.system('clear')
+    print("Congratulations!  You Won!!")
+    print("You correctly guessed the Word!")
+    print("It was {}".format(word.upper()))
+    return False
+
+
+def you_lose(word):
+    os.system('clear')
+    print("I'm sorry, but you've run out of guesses!")
+    print("The word was {}".format(word.upper()))
+    return False
+
 
 def welcome_rules():
-    print("Welcome to the Mystery Word Game!/nTry to guess the mystery word \
-    one letter at a time.")
+    print("Welcome to the Mystery Word Game!")
+    print("Guess the word one letter at a time.")
 
-def guess_limit():
+def play_again():
+    yes_no = input("Would you like to play again (y/n)? ").lower()
 
-def win_lose():
+    if yes_no not in "yn" or len(yes_no) > 1:
+        return play_again()
+    elif yes_no == "y":
+        return True
+    else:
+        return False
+
+
+def main():
+    welcome_rules()
+    words = choose_difficulty()
+    word = choose_word(words)
+
+    guess(word)
+
+    if play_again():
+        return main()
+    else:
+        os.system('clear')
+        return
+
+
+
+
+
 
 
 
