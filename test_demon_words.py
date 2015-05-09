da29516ab030baba193025144292bd4a54ce8de7
Benjamin Phillips <benred42@gmail.com>
2015-05-09 14:10:23 -0400

    fixed test_demon_words.py again

--- test_demon_words.py
+++ test_demon_words.py
@@ -4,14 +4,12 @@ word_list = ["foot", "feet", "hand", "head", "need", "kept", "knot", "than"]
 word_list2 = ["foot", "than", "feet", "then"]
 
 def test_choose_best_list():
-    assert choose_best_list(word_list, "o")[1] == ["feet", "hand", "head", "need", "kept", "than"]
-    assert choose_best_list(word_list, "t")[1] == ["foot", "feet", "kept", "knot"]
-    assert choose_best_list(word_list, "e")[1] == ["foot", "hand", "knot", "than"]
-    assert choose_best_list(word_list2, "t")[1] == ["than", "then"]
+    assert choose_best_list(word_list,"....", "o")[1] == ["feet", "hand", "head", "need", "kept", "than"]
+    assert choose_best_list(word_list,"....", "t")[1] == ["foot", "feet", "kept", "knot"]
+    assert choose_best_list(word_list,"....", "e")[1] == ["foot", "hand", "knot", "than"]
+    assert choose_best_list(word_list2,"....", "t")[1] == ["than", "then"]
 
 def test_merge_words():
     assert merge_words("t...", "..e.") == "T _ E _"
     assert merge_words("t...", "..e.", "." , "." , "" ,False) == "t.e."
     assert merge_words("t...", "t...") == "T _ _ _"
-    print("test", "halp")
-    assert merge_words("test", "halp") == "H A L P"
